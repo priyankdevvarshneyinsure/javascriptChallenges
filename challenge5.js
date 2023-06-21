e6abb47570c0d80dda26b5bf08dd512473e450b7
priyankdevvarshneyinsure <137282153+priyankdevvarshneyinsure@users.noreply.github.com>
2023-06-21 07:25:30 -0400

    Create challenge5.js

---
+++ challenge5.js
@@ -0,0 +1,40 @@
+Question :
+/* CHALLENGE 5 - Ternary Operator
+
+Change contents of the isNumber function
+using ternary operator.
+*/
+
+"use strict";
+
+function isNumber(a) {
+  if (typeof a === "number") {
+    return "That's number";
+  } else {
+    return "That's not a number";
+  }
+}
+
+console.log(isNumber(10));
+// That's number
+
+console.log(isNumber("Hey there"));
+// That's not a number
+
+console.log(isNumber(true));
+// That's not a number
+
+
+
+
+Solution:
+
+function isNumber(a){
+
+
+    return typeof a == "number" ? "that is number" : "not a number";
+  
+
+
+}
+console.log(isNumber(10));
