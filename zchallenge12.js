2753f261fa6e4e2c904aa2b55dbbfe33b650dc1c
priyankdevvarshneyinsure <137282153+priyankdevvarshneyinsure@users.noreply.github.com>
2023-06-22 07:03:57 -0400

    Create zchallenge12.js

---
+++ zchallenge12.js
@@ -0,0 +1,40 @@
+Question : /* CHALLENGE 12 - Copy Array
+
+Create copy of the a array.
+*/
+
+"use strict";
+
+var a = [1, 2, 3];
+
+var b;
+
+//Copy array here
+
+b.push("newElement");
+
+console.log(a);
+// [1, 2, 3]
+
+console.log(b);
+// [1, 2, 3, "newElement"]
+
+
+
+Solution : 
+
+"use strict";
+
+var a = [1, 2, 3];
+
+var b=[...a];
+
+//Copy array here
+
+b.push("newElement");
+
+console.log(a);
+// [1, 2, 3]
+
+console.log(b);
+// [1, 2, 3, "newElement"]
