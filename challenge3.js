fa57772d1503aba24b407912dd26b1505448ae7b
priyankdevvarshneyinsure <137282153+priyankdevvarshneyinsure@users.noreply.github.com>
2023-06-22 03:37:06 -0400

    Update challenge3.js

--- challenge3.js
+++ challenge3.js
@@ -1,1 +1,21 @@
+Question : /* CHALLENGE 3 - Let
+
+Change code to match output.
+*/
+
+"use strict";
+
+var i = 10;
+
+for (var i = 0; i < 5; i++) {
+  // some stuff
+  console.log(i);
+}
+
+console.log(i);
+// BEFORE: 5
+// AFTER: 10
+
+
+Solution: 
 use let in the for loop to get output as 10 so it not possible to use let i outside the for loop
