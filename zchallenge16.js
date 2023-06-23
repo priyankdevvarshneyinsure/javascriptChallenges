51b1db2a5cfcf102f33b4418841a5960c7dee5d9
priyankdevvarshneyinsure <137282153+priyankdevvarshneyinsure@users.noreply.github.com>
2023-06-23 04:51:50 -0400

    Create zchallenge16.js

---
+++ zchallenge16.js
@@ -0,0 +1,32 @@
+Question :
+
+/* CHALLENGE 16 - Swap values of the two variables
+
+Swap values of the a and b.
+Don't use for this any new variable.
+*/
+
+"use strict";
+
+var a = "first";
+var b = "second";
+
+// Write code here
+
+console.log(a, b);
+// second first
+
+
+
+Soluiton:
+
+
+var a = "first";
+var b = "second";
+
+
+[a,b] = [b,a];
+// Write code here
+
+console.log(a, b);
+// second first
